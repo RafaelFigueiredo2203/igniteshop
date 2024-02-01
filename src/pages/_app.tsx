a36e59503597f2ad87ef7a1cc88c549b79ed6279
RafaelFigueiredo2203 <rafaelfigueiedojunior7@gmail.com>
2024-02-01 19:08:01 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+
+import { Container, Header } from "@/styles/pages/app"
+import Image from "next/image"
+import logoImg from "../assets/logo.svg"
+
+globalStyles()
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+        
+      </Header>
+        
+      <Component {...pageProps} />
+      </Container>
+  )
+}
+
+export default App
