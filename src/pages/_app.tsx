0b87268942312997290ce6feff6991b2ca0e16f7
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-11-27 15:53:37 -0300

    Gerando build 2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import { ModalDrawer } from '@/components/Modal'
 import { Container, Header } from '@/styles/pages/app'
 import { CartProvider } from '@/utils/context/context'
 import { AppProps } from 'next/app'
@@ -7,6 +6,7 @@ import Link from 'next/link'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import logoImg from '../assets/logo.svg'
+import { ModalDrawer } from '../components/Modal'
 import { globalStyles } from '../styles/global'
 import '../styles/index.css'
 
